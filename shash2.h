6f29b7b3516d9579a7fd872366fb80d530099bc0
Nathan Kern <natek5309@gmail.com>
2017-11-17 10:51:48 -0500

    Basic flow, no locks

--- shash2.h
+++ shash2.h
@@ -6,11 +6,35 @@
 ///       contains the lock, so we can't just reuse the clist implementation.
 ///       In addition, the API now allows for multiple keys on each
 ///       operation.
+
 class shash2
 {
+private:
+    struct Node {
+        int val;
+        struct Node* next;
+    };
+
+    struct Sentinel {
+        std::mutex mtx;
+        struct Node* head;
+    };
+
+    // constant pointer to buckets, whose head values might change
+    const struct Sentinel* buckets;
+    const unsigned int numBuckets;
 public:
-	shash2(unsigned _buckets)
-	{}
+	shash2(unsigned int _buckets)
+	{
+        numBuckets = _buckets;
+        // allocate an array of Sentinels
+        buckets = new Sentinel[numBuckets];
+        // each bucket initially points to nothing
+        for (int i=0; i < numBuckets; ++i)
+        {
+            buckets[i].head = NULL;
+        }
+    }
 
 	/// insert /num/ values from /keys/ array into the hash, and return the
 	/// success/failure of each insert in /results/ array.
@@ -23,6 +47,89 @@ public:
 	/// return true if *key* is present in the list, false otherwise
 	void lookup(int* keys, bool* results, int num) const
 	{}
+
+    // only returns false if the value already exists
+    bool _insert(int key)
+    {
+        struct Node* current = buckets[key % numBuckets].head;
+        struct Node* previous = current;
+
+        struct Node* node = new Node;
+        node->val = key;
+        node->next = NULL;
+
+        if (current == NULL)
+        {
+            buckets[index].head = node;
+
+            return true;
+        }
+
+        while (current != NULL && key > current->val)
+        {
+            previous = current;
+            current = current->next;
+        }
+
+        if (current == NULL)
+        {
+            // we're at the end of the list
+            previous->next = node;
+            return true;
+        }
+
+        // does the value already exist?
+        if (current->value == key)
+        {
+            return false;
+        }
+
+        // we're somewhere in the middle
+        previous->next = node;
+        node->next = current;
+
+        return true;
+    }
+
+    bool _remove(int key)
+    {
+        struct Node* current = buckets[key % numBuckets].head;
+        struct Node* previous = current;
+
+        while (current != NULL && key > current->val)
+        {
+            previous = current;
+            current = current->next;
+        }
+
+        if (current == NULL)
+        {
+            // key is not in list
+            return false;
+        }
+
+        previous->next = current->next;
+        
+        delete current;
+        return true;
+    }
+
+    bool _lookup(int key) const
+    {
+        struct Node* current = buckets[index].head;
+        while (current != NULL && key > current->val)
+        {
+            current = current->next;
+        }
+
+        if (key == current->val)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
 
 	//The following are not tested by the given tester but are required for grading
 	//No locks are required for these.
